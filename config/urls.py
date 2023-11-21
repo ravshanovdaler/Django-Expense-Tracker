f0b68533e287e17c877c87d30f9c88d59360c97a
ravshanovdaler <ravshanovdaler06@gmail.com>
2023-11-21 15:51:32 +0500

    requirements

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('user/', include('users.urls')),
     path('user/', include('dj_rest_auth.urls')),
